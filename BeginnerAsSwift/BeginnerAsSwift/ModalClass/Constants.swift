d5f644c9615ac21aed22dfa4809b55881ff87728
Surbhi Natani <surbhi.natani@claritusconsulting.com>
2018-04-02 17:18:51 +0530

    fifth commit

---
+++ BeginnerAsSwift/BeginnerAsSwift/ModalClass/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ClaritusCoreSwift
+//
+//  Created by ionic on 13/03/18.
+//  Copyright © 2018 Claritus Consulting. All rights reserved.
+//
+
+import UIKit
+
+@objcMembers
+class Constants: NSObject {
+ static let APP_ALERT_TITLE = "Swift Constants"
+    
+    
+    
+    
+}
