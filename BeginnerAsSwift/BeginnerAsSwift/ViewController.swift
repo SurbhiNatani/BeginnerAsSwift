cc32fe924fa15a39583f887ea49c6e5d7d2afba5
Surbhi Natani <surbhi.natani@claritusconsulting.com>
2018-04-02 16:45:20 +0530

    first commit

---
+++ BeginnerAsSwift/BeginnerAsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeginnerAsSwift
+//
+//  Created by ionic on 02/04/18.
+//  Copyright © 2018 Claritus Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
